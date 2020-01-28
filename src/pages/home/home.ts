fd7229f2ef6c68ec4b551413e0e7020df682bfac
matías <mluna@goiar.com>
2020-01-28 17:56:14 -0300

    draw image based on its original orientation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,17 +9,60 @@ import * as exif from 'exif-js';
 export class HomePage {
 
   image: any;
+  imageConverted: string = 'asdf';
 
   constructor(public navCtrl: NavController) {
     
   }
 
   clicked(e) {
-    console.log('clicked');
-    exif.getData(e.target, function() {
-      console.log(e.target);
-      var allMetaData = exif.getAllTags(this);
-      console.log(allMetaData.Orientation);
+    exif.getData(e.target, () => {
+      let allMetaData = exif.getAllTags(e.target);
+
+      let MAX_WIDTH = 500;
+      let MAX_HEIGHT = 500;
+      let width = e.target.width;
+      let height = e.target.height;
+      let canvas = document.createElement('canvas');
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+      let ctx = canvas.getContext('2d');
+
+      switch(allMetaData.Orientation){
+        case 8:
+          // 90° rotate left
+          ctx.rotate(-0.5 * Math.PI);
+          ctx.translate(-width, 0);
+        break;
+        case 3:
+          // 180° rotate left
+          ctx.translate(width, height);
+          ctx.rotate(Math.PI);
+          break;
+        case 6:
+          // 90° rotate right
+        ctx.rotate(0.5 * Math.PI);
+        ctx.translate(0, -height);
+            break;
+     }
+
+    ctx.drawImage(e.target, 0, 0, width, height);
+    
+    this.imageConverted = canvas.toDataURL('image/jpeg', 80);
+
   });
   }
 
